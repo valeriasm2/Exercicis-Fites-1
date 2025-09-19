79ad1b24f9bddf39d1c8adbd4316a7237d37b474
valeria <vsantanamegia01.cf@iesesteveterradas.cat>
2025-09-19 17:06:08 +0200

    archivo sudoku creado y tabla

---
+++ sudoku.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Sudoku </title>
+	<style>
+		h1 {
+			text-align: center;
+		}
+		body {
+			font-family: Arial, sans-serif;
+			margin: 20px;
+		}
+		footer {
+		    position: fixed;
+		    bottom: 0;
+		    left: 0;
+		    width: 100%;
+		    padding: 10px;
+		    background: lemonchiffon;
+		    text-align: center;
+		}
+		table {
+			border-collapse: collapse;
+			margin: 20px auto;
+		}
+		td {
+			border: 1px solid black;
+			padding: 10px;
+			text-align: center;
+			width: 40px;
+			height: 40px;
+		}
+	</style>
+</head>
+<body>
+	<?php
+		echo "<h1>Juego Sudoku</h1>";
+		$F = 9; 
+		$C = 9;
+	?>
+	<table>
+		<?php
+			for ($i = 0; $i < $F; $i++) {
+			    echo "<tr>";
+			    for ($j = 0; $j < $C; $j++) {
+			        echo "<td></td>";
+			    }
+			    echo "</tr>";
+			}
+		?>
+	</table>
+
+	<footer>
+		<div>Footer</div>
+	</footer>
+</body>
+</html>
